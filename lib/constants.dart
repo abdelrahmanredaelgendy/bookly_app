5b4552ae6f2c18ffb025138f31b0aca2e9585031
Abdelrahman <abdelrahmanreda748@gmail.com>
2023-09-18 14:50:44 +0300

    navigate to HomeScreen and refactor code

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 
   import 'package:flutter/material.dart';
  const Color kPrimaryColor = Color(0xff100B20);
-
+ const Duration kTransitionDuration =Duration(milliseconds: 250);
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,6 +19,19 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   @override
   void initState() {
+    initSlidingAnimation();
+    navigateToHome();
+    super.initState();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(const HomeView(),
+          transition: Transition.fade, duration: kTransitionDuration);
+    });
+  }
+
+  void initSlidingAnimation() {
     animationController =
         AnimationController(vsync: this, duration: const Duration(seconds: 1));
     slidingAnimation = Tween<Offset>(
@@ -23,13 +39,14 @@ class _SplashViewBodyState extends State<SplashViewBody>
       end: Offset.zero,
     ).animate(animationController);
     animationController.forward();
-    super.initState();
   }
- @override
+
+  @override
   void dispose() {
     animationController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
